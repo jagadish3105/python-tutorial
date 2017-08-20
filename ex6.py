664fb3f7b99bd69721e654da7047289477e9720e
jagadish <jagadish3105@outlook.com>
2017-08-20 12:35:32 +0530

    strings and text

---
+++ ex6.py
@@ -0,0 +1,13 @@
+x="there are %d types of people." % 10
+binary="binary"
+do_not="dont"
+y="those who know %s and those who %s." % (binary, do_not)
+print (x)
+print (y)
+print("i said %r " % x)
+hilarious= False
+joke_evulation= "isn't that joke so funny/! %r"
+print(joke_evulation %hilarious)
+w="this is the left side of ...."
+e="a strindg with a right side."
+print(w+e)
